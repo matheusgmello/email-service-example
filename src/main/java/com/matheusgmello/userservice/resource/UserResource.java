e77fff924d39851ef81c42cd4b58e73515db900c
matheusgmello <matheusgmello1@gmail.com>
2023-07-05 14:29:02 -0300

    User Resource Get

--- src/main/java/com/matheusgmello/userservice/resource/UserResource.java
+++ src/main/java/com/matheusgmello/userservice/resource/UserResource.java
@@ -7,10 +7,7 @@ import com.matheusgmello.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.net.URI;
@@ -27,6 +24,12 @@ public class UserResource {
     public ResponseEntity<HttpResponse> createUser(@RequestBody User user) {
         User newUser = userService.saveUser(user);
         return ResponseEntity.created(URI.create("")).body(HttpResponse.builder().timeStamp(LocalDateTime.now().toString()).data(Map.of("user", newUser)).message("User created").status(HttpStatus.CREATED).statusCode(HttpStatus.CREATED.value()).build());
+    }
+
+    @GetMapping
+    public ResponseEntity<HttpResponse> confirmUserAccount(@RequestParam("token") String token) {
+        Boolean isSuccess = userService.verifyToken(token);
+        return ResponseEntity.ok().body(HttpResponse.builder().timeStamp(LocalDateTime.now().toString()).data(Map.of("Success", isSuccess)).message("Account Verified").status(HttpStatus.OK).statusCode(HttpStatus.OK.value()).build());
     }
 
 
